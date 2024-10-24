f5818fde917b3f49e4e371e5dbc5de976713c6b6
amantoor17 <amantoordpps@gmail.com>
2024-10-24 10:42:47 +0530

    portfolio changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -62,7 +62,7 @@ export const PROJECTS = [
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML", "CSS", "React", "Tailwind", "Router", "React Icons"],
-	githubPage: "https://lokeshloki3.github.io/reactportfolioanimate/",
+	githubPage: "https://amantoor17.github.io/animated-portfolio/",
   },
   {
     title: "Github Profile Finder",
